442c0519782226279a4eefc6abbca873a32cad39
Timothy Eldridge <timothy.a.eldridge@gmail.com>
2018-11-27 13:15:03 -0800

    update README

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-├── LICENSE
-├── data
-│   ├── processed
-│   └── raw
-└── protein-atlas
-    └── __init__.py
+## Installation
+
+Required Python Version: 3.7
+
+`pip` installation:
+   pip install kaggle requests clint numpy scipy
+
+
+## Directory Structure
+
+  ├── data
+  │   ├── processed
+  │   └── raw
+  └── protein-atlas
+      └── __init__.py
+
+
--- protein-atlas/__init__.py
+++ protein-atlas/__init__.py
@@ -1,28 +1,39 @@
 import os
+import requests
+from pathlib import Path
+from clint.textui import progress
 
-ROOT = os.path.join(os.path.dirname(__file__),os.pardir)
-DATA_URL = "https://www.kaggle.com/c/10418/download-all"
+PROJECT_ROOT       = os.path.join(os.path.dirname(__file__),os.pardir)
+
+DATA_PATH     = os.path.join(PROJECT_ROOT,"data")
+TRAIN_PATH    = os.path.join(PROJECT_ROOT,"data/raw/train")
+TEST_PATH     = os.path.join(PROJECT_ROOT,"data/raw/test")
+
+KAGGLE_DATA_URL    = "https://www.kaggle.com/c/10418/download-all"
+DOWNLOAD_FILE = os.path.join(DATA_PATH,"download.zip")
+
 
 def configure():
     """
-    Make the directory structure for project.
+    Make the project directory structure.
     """
-    dirs = [
-        "data",
-        "data/raw",
-        "data/processed",
-    ]
-    for dir in dirs:
-        os.makedirs(os.path.join(ROOT,dir))
+    for path in [DATA_PATH,TRAIN_PATH,TEST_PATH]:
+        Path(path).mkdir(parents=True,exist_ok=True)
+
 
 def get_data():
     """
     Download the Protein Atlas Dataset
     """
-    pass
-
-
+    r = requests.get(KAGGLE_DATA_URL, stream=True)
+    with open(DOWNLOAD_FILE, 'wb') as f:
+        total_length = int(r.headers.get('content-length'))
+        for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+            if chunk:
+                f.write(chunk)
+                f.flush()
 
 if __name__ == "__main__":
     configure()
-
+    get_data()
+    
