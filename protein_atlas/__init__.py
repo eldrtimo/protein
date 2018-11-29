cd9562dc7576eea4c98431a406897e95be51f370
Timothy Eldridge <timothy.a.eldridge@gmail.com>
2018-11-29 15:44:28 -0800

    unzip progress bar, outpdir dict, avoid redownloading

--- protein_atlas/__init__.py
+++ protein_atlas/__init__.py
@@ -1,60 +1,111 @@
 import os
-import requests
-import zipfile
 import subprocess
+import pandas as pd
 from pathlib import Path
+from zipfile import ZipFile
+from clint.textui import progress
 
-PROJECT_ROOT  = Path(__file__).parent.parent.resolve()
-RAW_DATA_PATH = PROJECT_ROOT.joinpath("data/raw")
-DOWNLOAD_PATH = PROJECT_ROOT.joinpath("data/raw/download")
-TRAIN_PATH    = PROJECT_ROOT.joinpath("data/raw/train")
-TEST_PATH     = PROJECT_ROOT.joinpath("data/raw/test")
+# Absolute path to project root directory
+ROOT  = Path(__file__).parent.parent.resolve()
 
-def configure():
-    """
-    Make the project directory structure.
-    """
-    for path in [DOWNLOAD_PATH,TRAIN_PATH,TEST_PATH,RAW_DATA_PATH]:
-        path.mkdir(parents=True,exist_ok=True)
+PATH = {
+    "root"  : ROOT,
+    "data"  : ROOT.joinpath("data"),
+    "raw"   : ROOT.joinpath("data/raw"),
+    "test"  : ROOT.joinpath("data/raw/test"),
+    "train" : ROOT.joinpath("data/raw/train"),
+}
 
-def download_data():
-    """
-    Download the Protein Atlas Dataset by calling the Kaggle command line API.
-    """
-    download_cmd = ["kaggle", "competitions", "download", "-c",
-                    "human-protein-atlas-image-classification", "-p",
-                    "{}".format(DOWNLOAD_PATH)]
-    subprocess.run(download_cmd)
+KAGGLE_FILE_TO_LOCAL = {
+    "train.csv"    : PATH["raw"].joinpath("train.csv"),
+    "test.zip"     : PATH["raw"].joinpath("test.zip"),
+    "train.zip"    : PATH["raw"].joinpath("train.zip")
+}
 
-def unzip_data():
-    """
-    Unzip test and train datasets from the Kaggle download.
-    """
-    zip_paths = [DOWNLOAD_PATH.joinpath(name) for name in ["train.zip","test.zip"]]
-    zip_dests = [TRAIN_PATH, TEST_PATH]
+ZIPFILE_TO_OUTPUTDIR = {
+    "test.zip" : PATH["test"],
+    "train.zip" : PATH["train"],
+}
 
-    for zip_path, zip_dest in zip(zip_paths,zip_dests):
-        print("Unzipping {}...".format(zip_path))
-        print("\tdestination: {}".format(zip_dest),end="\n")
-        dest_size = zip_dest.stat().st_size
-        if dest_size < 100:
-            zf = zipfile.ZipFile(zip_path)
-            zf.extractall(zip_dest)
-            zf.close()
-            print("\tdone.")
-        else:
-            print("\talready has size {}, skipping.".format(dest_size))
 
-def move_data():
+def get_protein_atlas_zip(zipname,clean=False,force=False):
     """
-    Move data into place.
+    Download a Kaggle competition data .zip archive named `zipname` and store it in
+    its own directory.
+
+    Parameters:
+        zipname : str
+            name of the kaggle .zip file
+        clean : bool, default False
+            If true, delete the .zip file after expanding
+        force : bool, default False
+            If true, download and decompress the .zip file even if the directory 
+            ZIPFILE_TO_DESTIONATION[zipfile] already exists.
+
+    Throws: 
+        subprocess.CalledProcessError:
+            Subprocess call to kaggle returned with nonzero exit status.
+        KeyError :
+            Lookup to KAGGLE_FILE_TO_LOCAL[zipname] or ZIPFILE_TO_OUTPUTDIR[zipname] 
+            failed.
     """
-    for csv_name in ["train.csv","test.csv"]:
-        csv_path = DOWNLOAD_PATH.joinpath(csv_name)
-        if csv_path.exists():
-            csv_path.rename(RAW_DATA_PATH.joinpath(csv_name))
+    outputdir = ZIPFILE_TO_OUTPUTDIR[zipname]
+    if outputdir.exists() and not force:
+        print("{}: Skipping, output directory {} already exists"
+              .format(zipname,str(outputdir)))
+    else:
+        download_cmd = [
+            "kaggle", "competitions", "download",
+            "human-protein-atlas-image-classification",
+            "-f", zipname,
+            "-p", str(KAGGLE_FILE_TO_LOCAL[zipname].parent)
+        ]
+        print(" ".join(download_cmd))
+        subprocess.run(download_cmd,check=True)
+           
+        with ZipFile(KAGGLE_FILE_TO_LOCAL[zipname]) as f:
+            outputdir = ZIPFILE_TO_OUTPUTDIR[zipname]
+            if not outputdir.exists(): outputdir.mkdir(parents=True,exist_ok=True)
+            members = f.infolist()
+            n_members = len(members)
+            label = "unzipping {} ".format(zipname)
+            with progress.Bar(label=label, expected_size=n_members) as bar:
+                for i, member in enumerate(members):
+                    if not outputdir.joinpath(member.filename).exists():
+                        f.extract(member, path=outputdir)
+                    bar.show(i)
+            f.close()
+
+        if clean:
+            KAGGLE_FILE_TO_LOCAL[zipname].unlink()
+
+def get_protein_atlas_csv(name):
+    """Download a Kaggle competition .csv file name `name` and store it in
+    PATH["raw"]
+    """
+    outputdir = KAGGLE_FILE_TO_LOCAL[name].parent
+    if not outputdir.exists():
+        outputdir.mkdir(parents=True,exist_ok=True)
+        
+    download_cmd = [
+        "kaggle", "competitions", "download",
+        "human-protein-atlas-image-classification",
+        "-f", name,
+        "-p", str(outputdir)
+    ]
+    print(" ".join(download_cmd))
+    subprocess.run(download_cmd,check=True)
+
+def get_protein_atlas_file(name, **kwds):
+    suffix = Path(name).suffix
+    if suffix == ".csv":
+        get_protein_atlas_csv(name)
+    elif suffix == ".zip":
+        get_protein_atlas_zip(name, **kwds)
+    else:
+        print("skipping {}: extension {} not recognized".format(name,suffix))
 
 if __name__ == "__main__":
-    configure()
-    unzip_data()
-    move_data()
+    for name in KAGGLE_FILE_TO_LOCAL.keys():
+        get_protein_atlas_file(name, clean=True)
+    
