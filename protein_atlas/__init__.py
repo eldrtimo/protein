15a07e15b8d4c1cf65ff9318d9b6704e63ac75ef
Timothy Eldridge <timothy.a.eldridge@gmail.com>
2018-12-05 16:19:28 -0800

    don't know what happened here

--- protein_atlas/__init__.py
+++ protein_atlas/__init__.py
@@ -1,9 +1,9 @@
+# Standard Library Imports
 import os
 import subprocess
-import pandas as pd
-
 from pathlib import Path
 from zipfile import ZipFile
+
 from clint.textui import progress
 
 import pandas as pd
@@ -11,125 +11,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from skimage.exposure import equalize_adapthist
-
-
-# Absolute path to project root directory
-ROOT  = Path(__file__).parent.parent.resolve()
-
-PATH = {
-    "root"  : ROOT,
-    "data"  : ROOT.joinpath("data"),
-    "raw"   : ROOT.joinpath("data/raw"),
-    "test"  : ROOT.joinpath("data/raw/test"),
-    "train" : ROOT.joinpath("data/raw/train"),
-}
-
-KAGGLE_FILE_TO_LOCAL = {
-    "train.csv"    : PATH["raw"].joinpath("train.csv"),
-    "test.zip"     : PATH["raw"].joinpath("test.zip"),
-    "train.zip"    : PATH["raw"].joinpath("train.zip")
-}
-
-ZIPFILE_TO_OUTPUTDIR = {
-    "test.zip" : PATH["test"],
-    "train.zip" : PATH["train"],
-}
-
-
-def get_protein_atlas_zip(zipname,clean=False,force=False):
-    """
-    Download a Kaggle competition data .zip archive named `zipname` and store it in
-    its own directory.
-
-    Parameters:
-        zipname : str
-            name of the kaggle .zip file
-        clean : bool, default False
-            If true, delete the .zip file after expanding
-        force : bool, default False
-            If true, download and decompress the .zip file even if the directory 
-            ZIPFILE_TO_DESTIONATION[zipfile] already exists.
-
-    Throws: 
-        subprocess.CalledProcessError:
-            Subprocess call to kaggle returned with nonzero exit status.
-        KeyError :
-            Lookup to KAGGLE_FILE_TO_LOCAL[zipname] or ZIPFILE_TO_OUTPUTDIR[zipname] 
-            failed.
-    """
-    outputdir = ZIPFILE_TO_OUTPUTDIR[zipname]
-    if outputdir.exists() and not force:
-        print("{}: Skipping, output directory {} already exists"
-              .format(zipname,str(outputdir)))
-    else:
-        download_cmd = [
-            "kaggle", "competitions", "download",
-            "human-protein-atlas-image-classification",
-            "-f", zipname,
-            "-p", str(KAGGLE_FILE_TO_LOCAL[zipname].parent)
-        ]
-        print(" ".join(download_cmd))
-        subprocess.run(download_cmd,check=True)
-           
-        with ZipFile(KAGGLE_FILE_TO_LOCAL[zipname]) as f:
-            outputdir = ZIPFILE_TO_OUTPUTDIR[zipname]
-            if not outputdir.exists(): outputdir.mkdir(parents=True,exist_ok=True)
-            members = f.infolist()
-            n_members = len(members)
-            label = "unzipping {} ".format(zipname)
-            with progress.Bar(label=label, expected_size=n_members) as bar:
-                for i, member in enumerate(members):
-                    if not outputdir.joinpath(member.filename).exists():
-                        f.extract(member, path=outputdir)
-                    bar.show(i)
-
-        if clean:
-            KAGGLE_FILE_TO_LOCAL[zipname].unlink()
-
-def get_protein_atlas_csv(name):
-    """Download a Kaggle competition .csv file name `name` and store it in
-    PATH["raw"]
-    """
-    outputdir = KAGGLE_FILE_TO_LOCAL[name].parent
-    if not outputdir.exists():
-        outputdir.mkdir(parents=True,exist_ok=True)
-        
-    download_cmd = [
-        "kaggle", "competitions", "download",
-        "human-protein-atlas-image-classification",
-        "-f", name,
-        "-p", str(outputdir)
-    ]
-    print(" ".join(download_cmd))
-    subprocess.run(download_cmd,check=True)
-
-def get_protein_atlas_file(name, **kwds):
-    suffix = Path(name).suffix
-    if suffix == ".csv":
-        get_protein_atlas_csv(name)
-    elif suffix == ".zip":
-        get_protein_atlas_zip(name, **kwds)
-    else:
-        print("skipping {}: extension {} not recognized".format(name,suffix))
-
 from sklearn.preprocessing import MultiLabelBinarizer
+
+from .install import PATH
 
 class ProteinAtlas():
     def __init__(self):
-        self.csvpath = KAGGLE_FILE_TO_LOCAL["train.csv"]
-        self.df = pd.read_csv(self.csvpath)
-        self.df = self.process_df(self.df)
-        # self.encoder = MultiLabelBinarizer(classes = np.arange(len(self.classes)))
-        # self.labels = self.encoder.fit_transform(self.df["Target"])
+        df          = pd.read_csv(self.csvpath).set_index("Id")
+        read_target = lambda s: np.array(s.split(" "),dtype=np.int32)
+        targets     = df["Target"].apply(read_target)
+        mlb         = MultiLabelBinarizer()
+        y           = mlb.fit_transform(targets.values)
+        index       = targets.index
+        columns     = pd.MultiIndex.from_product([self.classes],names=["Target"])
 
-        # for color in ["red","green","blue","yellow"]:
-        #     self.df[color] = self.df["Id"].apply(lambda id: ProteinAtlas.get_path(id,color))
+        self.labels = pd.DataFrame(y,index,columns)
 
-        # self.labels = self.df["Target"].apply(lambda l: np.array(l.split(" "),dtype=np.int32))
-        # self.y = self.encoder.fit_transform(self.labels)
+    def any(self,class_):
+        """
+        Return examples belonging to any of the classes in `class_`.
+
+        Parameters:
+
+            class_: int or iterable of ints
+
+                Integer index or indices of classes to get examples from.
+        """
+        mask = pd.DataFrame(self.labels.iloc[:,class_] == 1).any(axis = 1)
+        return self.labels.loc[mask]
+        
+    def all(self,class_):
+        """
+        Return examples belonging to all of the classes in `class_`.
+
+        Parameters:
+
+            class_: int or iterable of ints
+
+                Integer index or indices of classes to get examples from.
+        """
+        mask = pd.DataFrame(self.labels.iloc[:,class_] == 1).all(axis = 1)
+        return self.labels.loc[mask]
+
+    @property
+    def csvpath(self): return PATH["train.csv"]
 
     @property
     def channels(self):
+        """
+        Channels present in the Protein Atlas dataset.
+        """
         return [
             "Microtubules",
             "Antibody",
@@ -139,19 +70,32 @@ class ProteinAtlas():
 
     @property
     def n_channels(self):
+        """Number of channels in each image of the Protein Atlas dataset."""
         return len(self.channels)
     
     @property
-    def colors(self): return ["red", "green", "blue", "yellow"]
+    def channel_colors(self):
+        """
+        Color of each channel in the Protein Atlas dataset.  These are not
+        technically colors, just identifiers necessary to locate the path of
+        each .png file.
+        """
+        return ["red", "green", "blue", "yellow"]
 
     @property
-    def width(self): return 512
+    def width(self):
+        """Width of each image in the Protein Atlas dataset, in pixels"""
+        return 512
 
     @property
-    def height(self): return 512
+    def height(self):
+        """Height of each image in the Protein Atlas dataset, in pixels"""
+        return 512
 
     @property
-    def depth(self): return len(self.channels)
+    def depth(self):
+        """Number of channels in each pixel for images in the Protein Atlas dataset."""
+        return len(self.channels)
 
     @property
     def classes(self):
@@ -185,25 +129,24 @@ class ProteinAtlas():
             "Cytoplasmic bodies",
             "Rods & rings",
         ]
+
     @property
     def n_classes(self):
         return len(self.classes)
 
-    def process_df(self,df):
-        df = df.set_index("Id")
-        read_target = lambda s: np.array(s.split(" "),dtype=np.int32)
-        df["labels"] = df["Target"].apply(read_target)
-        return df        
-        
-    def get_path(self,id_,color):
-        return PATH["train"].joinpath("{}_{}.png".format(id_,color))
+    def get_path(self,id_,channel_ix):
+        channel_color = self.channel_colors[channel_ix]
+        return PATH["train"].joinpath("{}_{}.png".format(id_,channel_color))
 
     def get_image(self,id_):
-        img = np.zeros((self.width,self.height,self.depth))
-        for channel in range(self.depth):
-            channel_path = self.get_path(id_,self.colors[channel])
-            img[:,:,channel] = plt.imread(str(channel_path))
+        img = np.zeros((self.width,self.height,self.n_channels))
+        for channel_ix in range(self.n_channels):
+            channel_path = self.get_path(id_,channel_ix)
+            img[:,:,channel_ix] = plt.imread(str(channel_path))
         return img
+
+    def plot_intensities(self, img):
+        pass
 
     def imshow(self,id_, ax=None):
         if not ax:
@@ -212,35 +155,3 @@ class ProteinAtlas():
         img = self.get_image(id_)
         img = equalize_adapthist(img)
         ax.imshow(img)
-    
-
-def install():
-    for name in KAGGLE_FILE_TO_LOCAL.keys():
-        get_protein_atlas_file(name, clean=True)
-    
-
-if __name__ == "__main__":
-    atlas = ProteinAtlas()
-    from skimage.exposure import *
-    import skimage.io as io
-    
-    layer2cmap = {
-        "red"    : "Reds",
-        "green"  : "Greens",
-        "blue"   : "Blues",
-        "yellow" : "Purples",
-    }
-    
-    WIDTH = 512
-    HEIGHT = 512
-    DEPTH = 4
-    ID = 0
-    
-    img = np.zeros((WIDTH,HEIGHT,DEPTH))
-    for i, layer in enumerate(layer2cmap.keys()):
-        img[:,:,i] = plt.imread(str(atlas.df[layer][ID]))
-
-    io.use_plugin("pil")
-    io.imshow(img)
-
-    
---
+++ protein_atlas/install.py
@@ -0,0 +1,129 @@
+# Standard Library Imports
+import os
+import subprocess
+from zipfile import ZipFile
+
+# Package imports
+from pathlib import Path
+from clint.textui import progress
+
+# Root project directory, one level above the module directory.
+ROOT = Path(__file__).parent.parent.resolve()
+
+# Project data paths
+PATH = {
+    "root"      : ROOT,
+    "data"      : ROOT.joinpath("data"),
+    "raw"       : ROOT.joinpath("data/raw"),
+    "test"      : ROOT.joinpath("data/raw/test"),
+    "train"     : ROOT.joinpath("data/raw/train"),
+    "train.csv" : ROOT.joinpath("data/raw/train.csv"),
+    "test.zip"  : ROOT.joinpath("data/raw/test.zip"),
+    "train.zip" : ROOT.joinpath("data/raw/train.zip")
+}
+
+def get_protein_atlas_zip(zipname,clean=False,force=False):
+    """Download a Kaggle competition data .zip archive named `zipname` and store it
+    in its own directory whose name is the stem of `zipname`.
+
+    Parameters:
+
+        zipname : str
+
+            name of the kaggle .zip file. ***UNDEFINED BEHAVIOR WHEN zipname HAS NO FILE EXTENSION***
+
+        clean : bool, default False
+
+            If true, delete the .zip file after expanding
+
+        force : bool, default False
+
+            If true, download and decompress the .zip file even if the directory
+            ZIPFILE_TO_DESTIONATION[zipfile] already exists.
+
+    Throws: 
+
+        subprocess.CalledProcessError:
+
+            Subprocess call to kaggle returned with nonzero exit status.
+
+        KeyError :
+
+            PATH lookup failed.
+
+    """
+    outputdir_name = Path(zipname).stem
+    outputdir = PATH[ outputdir_name ]
+    if outputdir.exists() and not force:
+        print("{}: Skipping, output directory {} already exists"
+              .format(zipname,str(outputdir)))
+    else:
+        download_cmd = [
+            "kaggle", "competitions", "download",
+            "human-protein-atlas-image-classification",
+            "-f", zipname,
+            "-p", str(PATH[zipname].parent)
+        ]
+        print(" ".join(download_cmd))
+        subprocess.run(download_cmd,check=True)
+           
+        with ZipFile(PATH[zipname]) as f:
+            outputdir = PATH[zipname]
+            if not outputdir.exists():
+                outputdir.mkdir(parents=True,exist_ok=True)
+            members   = f.infolist()
+            n_members = len(members)
+            label     = "unzipping {} ".format(zipname)
+            with progress.Bar(label=label, expected_size=n_members) as bar:
+                for i, member in enumerate(members):
+                    if not outputdir.joinpath(member.filename).exists():
+                        f.extract(member, path=outputdir)
+                    bar.show(i)
+
+        if clean:
+            PATH[zipname].unlink()
+
+def get_protein_atlas_csv(name):
+    """Download a Kaggle competition .csv file name `name` and store it in
+    PATH["raw"]
+    """
+    outputdir = PATH[name].parent
+    if not outputdir.exists():
+        outputdir.mkdir(parents=True,exist_ok=True)
+        
+    download_cmd = [ "kaggle", "competitions", "download",
+                     "human-protein-atlas-image-classification", "-f", name,
+                     "-p", str(outputdir) ]
+
+    print(" ".join(download_cmd))
+    subprocess.run(download_cmd,check=True)
+
+def get_protein_atlas_file(name,clean=False):
+    """Download a Kaggle competition file `name` and store the file at
+    `PATH[name]`. If `name` is a .csv file, run get_protein_atlas_csv(name).  If
+    `name` is a .zip file, run `get_protein_atlas_zip(name)` and store the
+    contents of the archive at `PATH[name]`.
+
+    Patemeters:
+
+        name : string
+
+        clean : bool, default False
+
+            If `clean` is `True`, delete .zip files after unzipping.
+    """
+    suffix = Path(name).suffix
+    if suffix == ".csv":
+        get_protein_atlas_csv(name)
+    elif suffix == ".zip":
+        get_protein_atlas_zip(name,clean)
+    else:
+        print("skipping {}: extension {} not recognized".format(name,suffix))
+
+def install():
+    for name in KAGGLE_FILE_TO_.keys():
+        get_protein_atlas_file(name, clean=True)
+
+if __name__ == "__main__":
+    install()
+
